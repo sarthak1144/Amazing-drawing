95115de63751fabb42c5bd64e2f3f44aff9b21f9
Raj singh chauhan <67595118+rajsingh010@users.noreply.github.com>
2020-10-01 23:32:54 +0530

    Create tutle_flower.py
    a beautiful flower made with python, how you find it good

---
+++ tutle_flower.py
@@ -0,0 +1,66 @@
+import turtle
+x = turtle.Turtle()
+x.speed(70)
+l = 70
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("red")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("blue")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("green")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("yellow")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("orange")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+x.width(10)
+x.pencolor("green")
+x.lt(90)
+x.fd(250)
